6fcae51ec0d2475f4e88432e660956ef86d95078
Marcus Xavier <marcusxavierr123@gmail.com>
2020-05-29 16:57:18 -0300

    sepia finished

--- helpers.c
+++ helpers.c
@@ -24,11 +24,30 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    int red;
+    int green;
+    int blue;
+    RGBTRIPLE pixel;
     for(int i = 0; i < height; i++)
     {
         for(int j = 0; j < width; j++)
         {
+            pixel = image[i][j];
+            red = pixel.rgbtRed;
+            green = pixel.rgbtGreen;
+            blue = pixel.rgbtBlue;
 
+            image[i][j].rgbtRed = floor(//Red
+                (0.393 * red) + (0.769 * green) + (0.189 * blue)
+            ) > 255 ? 255:floor((0.393 * red) + (0.769 * green) + (0.189 * blue));
+
+            image[i][j].rgbtGreen = floor(//Green
+                (0.349 * red) + (0.686 * green) + (0.168 * blue)
+            ) > 255 ? 255:floor((0.349 * red) + (0.686 * green) + (0.168 * blue));
+            
+            image[i][j].rgbtBlue = floor(//Blue
+                (0.272 * red) + (0.534 * green) + (0.131 * blue)
+            ) > 255 ? 255:floor((0.272 * red) + (0.534 * green) + (0.131 * blue));
         }
     }
     return;
